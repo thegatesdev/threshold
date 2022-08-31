24b0705d09d0a00cd0101be9ea71d50c6ea43014
TheGatesScratch <mtkarels@outlook.com>
2022-08-31 21:30:32 +0200

    add fromTo utility

--- src/main/java/com/thegates/gatebase/GateBase.java
+++ src/main/java/com/thegates/gatebase/GateBase.java
@@ -1,6 +1,17 @@
 package com.thegates.gatebase;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
 
 public class GateBase extends JavaPlugin {
+
+    public Vector[] fromTo(Vector from, Vector to, double step) {
+        final int steps = (int) Math.ceil(from.distance(to) / step);
+        final Vector[] array = new Vector[steps];
+        final Vector dir = from.clone().subtract(to);
+        for (int i = 0; i <= steps; i++) {
+            array[i] = dir.clone().multiply(i * step);
+        }
+        return array;
+    }
 }
