51cfb1fa5e964600a68dbc509880082cc1574fb6
TheGatesScratch <mtkarels@outlook.com>
2022-10-27 20:10:27 +0200

    Add MathUtil.

---
+++ src/main/java/com/thegates/gatebase/util/MathUtil.java
@@ -0,0 +1,11 @@
+package com.thegates.gatebase.util;
+
+public class MathUtil {
+    public static double clampD(double val, double min, double max) {
+        return Math.max(min, Math.min(max, val));
+    }
+
+    public static int clampI(int val, int min, int max) {
+        return Math.max(min, Math.min(max, val));
+    }
+}
