9b5d52595f14a54e903e94c6e121c295f923be09
TheGatesScratch <mtkarels@outlook.com>
2022-10-27 19:51:14 +0200

    Rework loggable.

--- src/main/java/com/thegates/gatebase/Loggable.java
+++ src/main/java/com/thegates/gatebase/Loggable.java
@@ -1,13 +1,65 @@
 package com.thegates.gatebase;
 
-public interface Loggable {
-    void log(String message);
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Consumer;
 
-    default void log(String message, Level level) {
-        log(message);
+public final class Loggable {
+
+    private List<Message> noConsumerLogs;
+    private boolean hasListeners;
+    private final List<Consumer<Message>> consumers = new LinkedList<>();
+
+    public Loggable() {
+        noConsumerLogs = new LinkedList<>();
+        hasListeners = false;
     }
 
-    enum Level {
-        NORMAL, IMPORTANT, WARNING, ERROR
+    public Loggable(Consumer<Message> consumer) {
+        noConsumerLogs = null;
+        consumers.add(consumer);
+        hasListeners = true;
+    }
+
+    public void listen(Consumer<Message> consumer) {
+        consumers.add(consumer);
+        if (!hasListeners) {
+            noConsumerLogs.forEach(consumer);
+            noConsumerLogs = null;
+            hasListeners = true;
+        }
+    }
+
+    public void log(Message message) {
+        if (hasListeners) {
+            consumers.forEach(consumer -> consumer.accept(message));
+        } else noConsumerLogs.add(message);
+    }
+
+    public void log(String message) {
+        log(new Message(message, LogPrio.NORMAL));
+    }
+
+    public void log(String message, LogPrio prio) {
+        log(new Message(message, prio));
+    }
+
+    public record Message(String message, LogPrio prio) {
+        public String joined() {
+            return prio.name() + ": " + message;
+        }
+    }
+
+    enum LogPrio {
+        BOTTOM(0), LOW(1), NORMAL(2), HIGH(3), TOP(4);
+        public final int level;
+
+        LogPrio(int level) {
+            this.level = level;
+        }
+
+        public int compare(LogPrio other) {
+            return Integer.compare(level, other.level);
+        }
     }
 }
