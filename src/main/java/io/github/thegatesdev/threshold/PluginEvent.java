bdf8999d0761b10d84f9a1a306377ed4da4ef353
TheGatesScratch <mtkarels@outlook.com>
2023-04-25 11:36:02 +0200

    Implement dispatchAsync

--- src/main/java/io/github/thegatesdev/threshold/PluginEvent.java
+++ src/main/java/io/github/thegatesdev/threshold/PluginEvent.java
@@ -2,12 +2,31 @@ package io.github.thegatesdev.threshold;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 public class PluginEvent<T> {
     private final List<Listener<T>> listeners = new ArrayList<>();
 
     public void dispatch(T t) {
         for (int i = 0; i < listeners.size(); i++) listeners.get(i).handle(t);
+    }
+
+    public void dispatchAsync(T t, ExecutorService executor) {
+        final Future<?>[] futures = new Future[listeners.size()];
+        for (int i = 0; i < listeners.size(); i++) {
+            final int f = i;
+            futures[f] = executor.submit(() -> listeners.get(f).handle(t));
+        }
+        for (int i = 0, futuresLength = futures.length; i < futuresLength; i++) {
+            try {
+                futures[i].get();
+            } catch (InterruptedException ignored) {
+            } catch (ExecutionException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 
     public void bind(Listener<T> listener) {
