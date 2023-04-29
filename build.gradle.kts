6bd8e58c9462b2ecf156377ba4c8d7a6918d1f45
TheGatesScratch <mtkarels@outlook.com>
2023-04-29 15:13:09 +0200

    Improve build.gradle.kts, add description.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     `java-library`
-    `maven-publish`
     id("io.papermc.paperweight.userdev") version "1.5.4"
 }
 
@@ -10,15 +9,7 @@ description = "A plugin basis and utility"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 java {
-    // Configure the java toolchain. This allows gradle to auto-provision JDK 17 on systems that only have JDK 8 installed for example.
     toolchain.languageVersion.set(JavaLanguageVersion.of(17))
-}
-
-repositories {
-    mavenLocal()
-    maven {
-        url = uri("https://repo.maven.apache.org/maven2/")
-    }
 }
 
 dependencies {
@@ -36,19 +27,12 @@ tasks {
     }
 
     compileJava {
-        options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
-
-        // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
-        // See https://openjdk.java.net/jeps/247 for more information.
+        options.encoding = Charsets.UTF_8.name()
         options.release.set(17)
     }
 
-    javadoc {
-        options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
-    }
-
     processResources {
-        filteringCharset = Charsets.UTF_8.name() // We want UTF-8 for everything
+        filteringCharset = Charsets.UTF_8.name()
         val props = mapOf(
             "name" to project.name,
             "version" to project.version,
