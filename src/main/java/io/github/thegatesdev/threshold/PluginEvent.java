5549cf93c6622becd6c1ff4cfb7cd2555706e533
TheGatesScratch <mtkarels@outlook.com>
2023-05-03 17:51:26 +0200

    Improve error handling

--- src/main/java/io/github/thegatesdev/threshold/PluginEvent.java
+++ src/main/java/io/github/thegatesdev/threshold/PluginEvent.java
@@ -2,15 +2,30 @@ package io.github.thegatesdev.threshold;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
+import java.util.function.Consumer;
 
 public class PluginEvent<T> {
     private final List<Listener<T>> listeners = new ArrayList<>();
+    private final Consumer<Exception> errorHandler;
+
+    public PluginEvent(Consumer<Exception> errorHandler) {
+        this.errorHandler = errorHandler;
+    }
+
+    public PluginEvent() {
+        this(null);
+    }
 
     public void dispatch(T t) {
-        for (int i = 0; i < listeners.size(); i++) listeners.get(i).handle(t);
+        for (int i = 0, size = listeners.size(); i < size; i++) {
+            try {
+                listeners.get(i).handle(t);
+            } catch (Exception e) {
+                if (errorHandler != null) errorHandler.accept(e);
+            }
+        }
     }
 
     public void dispatchAsync(T t, ExecutorService executor) {
@@ -23,8 +38,8 @@ public class PluginEvent<T> {
             try {
                 futures[i].get();
             } catch (InterruptedException ignored) {
-            } catch (ExecutionException e) {
-                throw new RuntimeException(e);
+            } catch (Exception e) {
+                if (errorHandler != null) errorHandler.accept(e);
             }
         }
     }
